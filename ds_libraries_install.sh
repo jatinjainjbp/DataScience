9f5bf0df7791f81c0afcff597b92c9281882b841
muralirk17 <murali.rk.2017@gmail.com>
2017-06-29 15:15:46 -0700

    ds_libraries_install.sh
    Install Datascience libraries using conda.

---
+++ ds_libraries_install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+echo "Installing Beautiful Soup package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge beautifulsoup4
+
+echo "Installing distance package"
+sudo /usr/bin/anaconda/bin/conda install -y -c moustik distance
+
+echo "Installing biopython package"
+sudo /usr/bin/anaconda/bin/conda install -y -c bioconda biopython
+
+echo "Installing docx2txt package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge docx2txt
+
+echo "Installing ghostscript package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge ghostscript
+
+echo "Installing imagemagick package"
+sudo /usr/bin/anaconda/bin/conda install -y -c vdbwrair imagemagick
+
+echo "Installing nltk package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge nltk
+
+echo "Installing opencv3 package"
+sudo /usr/bin/anaconda/bin/conda install -y -c menpo opencv3
+
+echo "Installing pypdf2 package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge pypdf2
+
+echo "Installing python-magic package"
+sudo /usr/bin/anaconda/bin/conda install -y -c birdhouse python-magic
+
+echo "Installing tensorflow package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge tensorflow
+
+echo "Installing tesseract package"
+sudo /usr/bin/anaconda/bin/conda install -y -c bioconda tesseract
+
+echo "Installing pdfminer package"
+sudo /usr/bin/anaconda/bin/conda install -y -c conda-forge pdfminer
